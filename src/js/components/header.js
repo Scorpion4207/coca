5329267e3f5621ee00e6cad953a45b19e044ccd5
Scorpion4207 <telitsin.andrei2015@yandex.ru>
2024-05-20 15:38:56 +0700

    Created hero section and fixed

---
+++ src/js/components/header.js
@@ -0,0 +1,24 @@
+export const headerScroll = () => {
+  const header = document.querySelector('[data-header="header"]') // находим header и записываем в константу
+
+  let prevScroll = window.scrollY // узнаем на сколько была прокручена страница ранее
+  let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+
+  document.addEventListener('scroll', () => { // при прокрутке страницы
+
+    currentScroll = window.scrollY // узнаем на сколько прокрутили страницу
+
+    const headerHidden = () => header.classList.contains('header--hidden') // узнаем скрыт header или нет
+
+    if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
+      header.classList.add('header--hidden') // то скрываем header
+    }
+    if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
+      header.classList.remove('header--hidden') // то отображаем header
+    }
+
+    prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
+
+  })
+  
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import '../assets/scss/main.scss';
 
 import { useTheme } from './components/theme.js';
+import { headerScroll } from './components/header.js';
 import { useBurger } from './components/burger.js';
 
 useTheme();
+headerScroll();
 useBurger();
