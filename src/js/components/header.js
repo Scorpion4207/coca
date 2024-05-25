4e951ef213b2b610b7d8349f96e165732b79618e
Scorpion4207 <telitsin.andrei2015@yandex.ru>
2024-05-25 11:36:28 +0700

    Added format script

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,24 +1,25 @@
 export const headerScroll = () => {
-  const header = document.querySelector('[data-header="header"]') // находим header и записываем в константу
+  const header = document.querySelector('[data-header="header"]'); // находим header и записываем в константу
 
-  let prevScroll = window.scrollY // узнаем на сколько была прокручена страница ранее
-  let currentScroll // на сколько прокручена страница сейчас (пока нет значения)
+  let prevScroll = window.scrollY; // узнаем на сколько была прокручена страница ранее
+  let currentScroll; // на сколько прокручена страница сейчас (пока нет значения)
 
-  document.addEventListener('scroll', () => { // при прокрутке страницы
+  document.addEventListener('scroll', () => {
+    // при прокрутке страницы
 
-    currentScroll = window.scrollY // узнаем на сколько прокрутили страницу
+    currentScroll = window.scrollY; // узнаем на сколько прокрутили страницу
 
-    const headerHidden = () => header.classList.contains('header--hidden') // узнаем скрыт header или нет
+    const headerHidden = () => header.classList.contains('header--hidden'); // узнаем скрыт header или нет
 
-    if (currentScroll > prevScroll && !headerHidden()) { // если прокручиваем страницу вниз и header не скрыт
-      header.classList.add('header--hidden') // то скрываем header
+    if (currentScroll > prevScroll && !headerHidden()) {
+      // если прокручиваем страницу вниз и header не скрыт
+      header.classList.add('header--hidden'); // то скрываем header
     }
-    if (currentScroll < prevScroll && headerHidden()) { // если прокручиваем страницу вверх и header скрыт
-      header.classList.remove('header--hidden') // то отображаем header
+    if (currentScroll < prevScroll && headerHidden()) {
+      // если прокручиваем страницу вверх и header скрыт
+      header.classList.remove('header--hidden'); // то отображаем header
     }
 
-    prevScroll = currentScroll // записываем на сколько прокручена страница на данный момент
-
-  })
-  
+    prevScroll = currentScroll; // записываем на сколько прокручена страница на данный момент
+  });
 };
